2754ae3d72fdc1c4e5e5438f47935ae932c4e2bb
Cory Loughran <loughrancory@gmail.com>
2020-04-24 12:03:24 -0400

    Added classification methods in NerualNet class, created a sample main program

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,56 +1,75 @@
 import numpy as np
 
+
 class NeuralNet:
 
-    def __init__(self, layers, inputs, outputs):
-        self.layers = layers
-        self.inputs = inputs
-        self.outputs = outputs
+    def __init__(self, inputs, outputs):
+        self._inputs = inputs
+        self._outputs = outputs
+        self.trained_iterations = 0
 
-        self.input_size = len(self.inputs[0])
+        self.input_size = len(self._inputs[0])
+        self.output_size = len(self._outputs[0])
 
-        self.hidden_weights = 2 * np.random.random((self.input_size, 128)) - 1
-        self.hidden_weights2 = 2 * np.random.random((128, 16)) - 1
-        self.output_weights = 2 * np.random.random((16, 10)) - 1
+        self._hidden_weights = 2 * np.random.random((self.input_size, 128)) - 1
+        self._hidden_weights2 = 2 * np.random.random((128, 16)) - 1
+        self._output_weights = 2 * np.random.random((16, self.output_size)) - 1
 
-    def train(self, n):
-        trained_outputs = self.outputs
+    def train(self, iterations):
+        trained_outputs = self._outputs
 
-        for i in range (n):
+        for i in range(iterations):
             # Times input by weights, then normalize using sigmoid
             # on both hidden and output layers
-            trained_hidden1 = self.sigmoid(np.dot(self.inputs, self.hidden_weights))
-            trained_hidden2 = self.sigmoid(np.dot(trained_hidden1, self.hidden_weights2))
-            trained_outputs = self.sigmoid(np.dot(trained_hidden2, self.output_weights))
+            trained_hidden1 = self.sigmoid(np.dot(self._inputs, self._hidden_weights))
+            trained_hidden2 = self.sigmoid(np.dot(trained_hidden1, self._hidden_weights2))
+            trained_outputs = self.sigmoid(np.dot(trained_hidden2, self._output_weights))
 
             # Caluclate error and adjusment on output layer
-            o_error = trained_outputs - self.outputs
+            o_error = trained_outputs - self._outputs
             o_adjustments = o_error * self.sigmoid_derivative(trained_outputs)
 
             # Calculate error and adjustment on hidden layers
-            h_error2 = np.dot(o_adjustments, self.output_weights.T)
+            h_error2 = np.dot(o_adjustments, self._output_weights.T)
             h_adjustments2 = h_error2 * self.sigmoid_derivative(trained_hidden2)
 
-            h_error = np.dot (h_error2 , self.hidden_weights2.T)
+            h_error = np.dot(h_error2, self._hidden_weights2.T)
             h_adjustments1 = h_error * self.sigmoid_derivative(trained_hidden1)
 
             # Back propogate and adjust weights
-            w1 = np.dot(self.inputs.T, h_adjustments1)
+            w1 = np.dot(self._inputs.T, h_adjustments1)
             w2 = np.dot(trained_hidden1.T, h_adjustments2)
             w3 = np.dot(trained_hidden2.T, o_adjustments)
 
-            self.hidden_weights -= 0.0001 * w1
-            self.hidden_weights2 -= 0.0001 * w2
-            self.output_weights -= 0.0001 * w3
+            self._hidden_weights -= 0.0001 * w1
+            self._hidden_weights2 -= 0.0001 * w2
+            self._output_weights -= 0.0001 * w3
             print(i)
+
+        self.trained_iterations = iterations
 
         return trained_outputs
 
-
-    def sigmoid(self, x):
-        a = 1 / (1 + np.exp (-x))
+    @staticmethod
+    def sigmoid(x):
+        a = 1 / (1 + np.exp(-x))
         return a
 
-
-    def sigmoid_derivative(self, x):
+    @staticmethod
+    def sigmoid_derivative(x):
         return x * (1 - x)
+
+    def classify(self, image):
+        classified_hidden = self.sigmoid(np.dot(image, self._hidden_weights))
+        classified_hidden2 = self.sigmoid(np.dot(classified_hidden, self._hidden_weights2))
+        classified_output = np.around(self.sigmoid(np.dot(classified_hidden2, self._output_weights)), decimals=2)
+
+        return classified_output
+
+    def bulk_classify(self, images):
+        classified_outputs = []
+
+        for image in images:
+            classified_outputs.append(self.classify(image))
+
+        return classified_outputs
--- main.py
+++ main.py
@@ -1,49 +1,47 @@
+from NeuralNet import NeuralNet
 import numpy as np
 import pickle
-from NeuralNet import NeuralNet
+from matplotlib import pyplot as plt
 
-#########################################
-# Load in the image arrays for the
-# training and test data from mnist.pkl
-#########################################
 def load():
     with open("mnist.pkl", 'rb') as f:
         mnist = pickle.load(f)
     return mnist["training_images"], mnist["training_labels"], mnist["test_images"], mnist["test_labels"]
 
-
-##############################################
-# Import and format data, train network
-##############################################
-
-# Load the training and test data
-x_train, t_train, x_test, t_test = load()
-
-# Format input and output data for training
-inputs = x_train/256
+x_train, y_train, x_test, y_test = load()
 
 outputs = np.zeros((60000, 10), dtype=int)
 for i in range(60000):
-    x = t_train[int(i)]
+    x = y_train[int(i)]
     outputs[i][x] = 1
 
-# Train network
-neural_net = NeuralNet(2, inputs, outputs)
-trained_set = neural_net.train(100)
-trained_set = np.around(trained_set, decimals=2)
+net = NeuralNet(x_train, outputs)
+
+first_image = x_test[9998]
+first_image = np.array(first_image, dtype='uint8')
+pixels = first_image.reshape((28, 28))
+plt.imshow(pixels, cmap='gray')
+plt.show()
+
+# for i in range(28):
+#     for j in range(28):
+#
+#         if pixels[i][j] < 128:
+#             print(" ", end="\t")
+#         else:
+#             print("@", end="\t")
+#     print()
+
+trained = net.train(100)
 
 
-##############################################
-# Calculate training accuracy and print
-# final iteration training results
-##############################################
-accuracy = 0
-for i in range(60000):
-    trained = np.rint(trained_set[i]).astype(int)
-    actual = outputs[i]
-    same = False
-    if np.array_equal(trained, actual):
-        accuracy += 1
-        same = True
-    # print(i, trained, actual, same)
-print(accuracy/60000)
+images = net.bulk_classify(x_test)
+
+properly_classified = 0
+
+for i in range(len(y_test)):
+    print(images[i], y_test[i], np.argmax(images[i]) == y_test[i])
+    if np.argmax(images[i]) == y_test[i]:
+        properly_classified += 1
+
+print(properly_classified / len(y_test))
